568f6eed740693e05b493297c92c9860218aef04
GarbleCM <1010311620@qq.com>
2022-03-07 11:03:33 +0800

    调整项目结构

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-
+  <div id="app">
+    <h1>黑马头条</h1>
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'App'
 }
 </script>
-
-<style>
+<style lang="less" scoped>
 
 </style>
